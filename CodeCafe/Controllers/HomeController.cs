8ff07cd699860ec1719284343fabbf8ddff2bf88
Leia Decker <lmdecker@dmacc.edu>
2021-04-18 20:04:09 -0500

    MVC addition

---
+++ CodeCafe/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeCafe.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- CodeCafe/Startup.cs
+++ CodeCafe/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodeCafe
 {
@@ -23,6 +24,14 @@ namespace CodeCafe
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+
+
+			services.AddRouting(options =>
+			{
+				options.LowercaseUrls = true;
+				options.AppendTrailingSlash = true;
+			});
+
 			services.AddControllersWithViews();
 		}
 
