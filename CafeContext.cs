07ab50b0a3b54d329009e913bf392fe3c3673358
gdecker2002 <77694417+gdecker2002@users.noreply.github.com>
2021-04-27 20:29:50 -0500

    Add files via upload

---
+++ CafeContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using CodeCafe.Models.SeedData;
+
+namespace CodeCafe.Models
+{
+    public class CafeContext : DbContext
+    {
+        public CafeContext(DbContextOptions<CafeContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Setting primary key
+            modelBuilder.Entity<OrderItem>().HasKey(oi => new { oi.ProductId });
+            //Setting foreign key
+            modelBuilder.Entity<OrderItem>().HasOne(oi => oi.Product)
+                .WithMany(p => p.OrderItems)
+                .HasForeignKey(oi => oi.ProductId);
+
+            modelBuilder.ApplyConfiguration(new SeedProducts());
+            modelBuilder.ApplyConfiguration(new SeedOrderItems());
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeCafe.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
